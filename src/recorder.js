bc02bc6be36bdf2ecf2c2d4dee55ce18307a1b4b
Casey Webb <notcaseywebb@gmail.com>
2020-07-27 13:27:32 -0500

    log on record

--- src/recorder.js
+++ src/recorder.js
@@ -3,16 +3,20 @@ const sensor = require('ds18x20')
 const { DATA_FILE, UPDATE_INTERVAL } = process.env
 
 exports.start = () => {
-  setInterval(() => append(sensor.getAll()), UPDATE_INTERVAL * 60 * 1000)
+  setInterval(recordCurrentTemps, UPDATE_INTERVAL * 60 * 1000)
 }
 
 exports.getData = () => {
   return fs.readJSON(DATA_FILE)
 }
 
-async function append(current) {
+async function recordCurrentTemps() {
+  const current = sensor.getAll()
   const data = fs.existsSync(DATA_FILE) ? await fs.readJSON(DATA_FILE) : {}
   const time = Date.now()
+
+  console.log(current)
+
   Object.keys(current).forEach((sensorId, temp) => {
     const entry = {
       time,
