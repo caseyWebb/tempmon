09e5e5f9cd206b214b43f4079bc834db57234e45
Casey Webb <notcaseywebb@gmail.com>
2020-07-28 11:38:13 -0500

    disable caching

--- src/server.js
+++ src/server.js
@@ -7,6 +7,11 @@ const app = express()
 const { DATA_FILE, PORT } = process.env
 
 exports.start = () => {
+  app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store')
+    next()
+  })
+
   app.use(express.static(path.resolve(__dirname, '../public')))
   app.get('/data', (req, res) => res.sendFile(DATA_FILE))
   app.get('/current', (req, res) => res.json(sensor.getAll()))
