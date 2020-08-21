e3be97157a5ffe40c22a5b30b39444b6bb0ad8f4
Casey Webb <notcaseywebb@gmail.com>
2020-08-21 17:58:12 -0500

    feat: nice up/down ux

--- src/client/components/app/index.ts
+++ src/client/components/app/index.ts
@@ -39,6 +39,10 @@ ko.components.register('app', {
       let timeout: number
       let prev = window.scrollY
       let scrollingUp = true
+      const pages = Array.from(
+        document.getElementsByClassName('page')
+      ).reverse() as HTMLElement[]
+      const vh = (p: number): number => (p * window.outerHeight) / 100
 
       snapToPage(scrollingUp)
 
@@ -46,22 +50,35 @@ ko.components.register('app', {
         scrollingUp = window.scrollY < prev
         prev = window.scrollY
         if (timeout) window.clearTimeout(timeout)
-        timeout = window.setTimeout(() => snapToPage(scrollingUp), 100)
+        timeout = window.setTimeout(() => snapToPage(scrollingUp), 50)
       })
+
+      document.addEventListener('keydown', (e) => {
+        // page up/down, arrow keys, spacebar
+        const up =
+          e.keyCode === 33 ||
+          e.keyCode === 38 ||
+          (e.keyCode === 32 && e.shiftKey)
+        const down =
+          e.keyCode === 34 ||
+          e.keyCode === 40 ||
+          (e.keyCode === 32 && !e.shiftKey)
+        if (!up && !down) return
+        e.preventDefault()
+        const i =
+          pages.findIndex((p) => p.offsetTop - window.scrollY < vh(50)) +
+          (up ? 1 : -1)
+        if (i >= pages.length || i < 0) return
+        const page = pages[i]
+        window.scrollTo({ behavior: 'smooth', top: page.offsetTop - 50 })
+      })
+
+      function snapToPage(scrollingUp: boolean): void {
+        const threshold = scrollingUp ? vh(20) : vh(80)
+        const page = pages.find((p) => p.offsetTop - window.scrollY < threshold)
+        if (page)
+          window.scrollTo({ behavior: 'smooth', top: page.offsetTop - 50 })
+      }
     }
   },
 })
-
-function snapToPage(scrollingUp: boolean): void {
-  const viewportHeight = window.outerHeight
-  const pages = Array.from(
-    document.getElementsByClassName('page')
-  ) as HTMLElement[]
-  const tenPercent = viewportHeight / 10
-  const threshold = scrollingUp ? tenPercent * 2 : tenPercent * 8
-  const page = Array.from(pages)
-    .reverse()
-    .find((p) => p.offsetTop - window.scrollY < threshold)
-
-  if (page) window.scrollTo({ behavior: 'smooth', top: page.offsetTop - 50 })
-}
