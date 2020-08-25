6c3f30e3c2060a0617b1533bf8b144d04e8635ae
Casey Webb <notcaseywebb@gmail.com>
2020-08-25 16:03:18 -0500

    feat: use atomic write so JSON doesn't get corrupted

--- src/server/data.ts
+++ src/server/data.ts
@@ -7,10 +7,11 @@
  * ]
  */
 
+import { existsSync as fileExists, promises as fs } from 'fs'
 import path from 'path'
 
 import { SensorData } from 'ds18x20'
-import fs from 'fs-extra'
+import writeJSONFile from 'write-json-file'
 
 import { DATA_DIRECTORY } from './config'
 
@@ -18,9 +19,11 @@ export async function append(values: SensorData): Promise<void> {
   const now = new Date()
   const [yyyy, M, d] = [now.getFullYear(), now.getMonth(), now.getDate()]
   const file = path.resolve(DATA_DIRECTORY, `${yyyy}-${M}-${d}.json`)
-  const data = fs.existsSync(file) ? await fs.readJSON(file) : []
+  const data = fileExists(file)
+    ? JSON.parse(await fs.readFile(file, 'utf-8'))
+    : []
   const entry = [now.getTime(), values]
   console.info('Appending entry:', entry)
   data.push(entry)
-  await fs.outputJSON(file, data)
+  await writeJSONFile(file, data)
 }
